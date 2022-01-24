820183847660ee908561c9fdda2a99a307db8f3d
cosarmuhammet <92250713+cosarmuhammet@users.noreply.github.com>
2022-01-24 15:01:49 +0300

    Seek Bar Kotlin

---
+++ Seek_Bar/app/src/main/java/com/example/seek_bar/MainActivity.kt
@@ -0,0 +1,37 @@
+package com.example.seek_bar
+
+import android.os.Bundle
+import android.widget.SeekBar
+import androidx.appcompat.app.AppCompatActivity
+import kotlinx.android.synthetic.main.activity_main.*
+
+class MainActivity : AppCompatActivity() {
+
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_main)
+
+        seekBar.setOnSeekBarChangeListener(object : SeekBar.OnSeekBarChangeListener {
+
+            override fun onProgressChanged(seek: SeekBar, progress: Int, fromUser: Boolean) {
+
+                progressBar.setProgress(progress)
+                textView.setText(""+ progress +"%")
+
+            }
+
+            override fun onStartTrackingTouch(seek: SeekBar) {
+
+            }
+
+            override fun onStopTrackingTouch(seek: SeekBar) {
+
+            }
+        })
+
+
+    }
+}
+
+
